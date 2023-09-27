fc8e8dbbfac1f495e681b66619a0e84a1756046a
Ricardo Jimenez <124076987+RJDPI@users.noreply.github.com>
2023-09-27 06:03:04 +0000

    added 3 files under views

--- app.rb
+++ app.rb
@@ -1,9 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'open-uri'
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/symbols"
+  raw_data = URI.open(api_url).read
+  parsed_data = JSON.parse(raw_data)
+  @symbols = parsed_data.fetch("symbols").keys
+
+  erb(:index)
+end
+
+get("/:symbols") do
+   @original_currency = params.fetch("from_currency")
+
+    api_url = "https://api.exchangerate.host/symbols"
+    raw_data = URI.open(api_url).read
+    parsed_data = JSON.parse(raw_data)
+    @symbols = parsed_data.fetch("symbols").keys
+
+    erb(:convert1)
+end
+
+get("/:symbols/:tocurrency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?from=#{@original_currency}&to=#{@destination_currency}"
+  raw_data = URI.open(api_url).read
+  parsed_data = JSON.parse(raw_data)
+  
+  @exchange_rate = parsed_data.fetch("info").fetch("rate")
+  
+  erb(:convert2)
 end
